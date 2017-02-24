bd56de9f243125b6782a26c4653787d3b5952c84
Anne Morrow <anne.moore2008@gmail.com>
2017-02-24 16:18:01 -0700

    can pass saved question data to questions.html

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ import sqlite3
 from functools import wraps
 
 from flask import Flask, flash, redirect, render_template, \
-    request, session, url_for
+    request, session, url_for, g
     
 
 # config
@@ -22,7 +22,7 @@ def connect_db():
 
 def login_required(test):
     @wraps(test)
-    def wrap(*args, **kwars):
+    def wrap(*args, **kwargs):
         if 'logged_in' in session:
             return test(*args, **kwargs)
         else:
@@ -50,3 +50,28 @@ def login():
             flash('Welcome!')
             return redirect(url_for('questions'))
     return render_template('login.html')
+
+@app.route('/questions/')
+@login_required
+def questions():
+    g.db = connect_db()
+    cur = g.db.execute(
+        '''select list_name from lists where user_id is 
+        (select user_id from users where username is "admin")'''
+    )
+    question_lists = [
+        row[0] for row in cur.fetchall()
+    ]
+    cur = g.db.execute(
+        ''' select list_id, question, answer from questions'''
+    )
+    questions = [[row[0], str(row[1]), str(row[2])] for row in cur.fetchall()]
+    for question in questions:
+        cur = g.db.execute('select list_name from lists where list_id is ' + str(question[0]))
+        question[0] = str(cur.fetchall()[0][0])
+    g.db.close()
+    return render_template(
+        'questions.html',
+        question_lists=question_lists,
+        questions=questions
+    )
