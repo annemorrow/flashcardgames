6ead1ff53a7b9488c5a3aaa82ec5a61b7e48bb32
Anne Morrow <anne.moore2008@gmail.com>
2017-03-06 16:00:21 -0700

    add short_answer_question to models and db

--- db_create.py
+++ db_create.py
@@ -1,12 +1,17 @@
 from views import db
-from models import Subject
+from models import Subject, Short_Answer_Question
 
 # create the database and the db table
 db.create_all()
 
 # insert data
-db.session.add(Subject("Math"))
-db.session.add(Subject("English"))
+math = Subject("Math")
+english = Subject("English")
+db.session.add(math)
+db.session.add(english)
+
+db.session.add(Short_Answer_Question(math, "2 X 4", "8"))
+db.session.add(Short_Answer_Question(math, "9 X 20"))
 
 # commit the changes
 db.session.commit()
--- models.py
+++ models.py
@@ -3,8 +3,30 @@
 
 from views import db
 
+class Short_Answer_Question(db.Model):
+
+    __tablename__ = 'short_answer_questions'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text)
+    
+    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'))
+    subject = db.relationship('Subject',
+        backref=db.backref('short_answer_questions', lazy='dynamic'))
+        
+    def __init__(self, subject, question, answer=None):
+        self.question = question
+        self.answer = answer
+        self.subject = subject
+    
+    def __repr__(self):
+        return 'SAQuestion %r>' % self.question[:10]
+
 
 class Subject(db.Model):
+
+    __tablename__ = 'subjects'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
